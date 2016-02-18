98a14e91f6f85b3407e24abce42019b1771eadbd
David Freeman <d526f861@cougar.cis.ksu.edu>
2016-02-17 20:46:31 -0600

    Initial Cube and Rotate code

---
+++ Rotate.java
@@ -0,0 +1,46 @@
+public class Rotate{
+
+	public void front(Cube cube){
+
+	}
+
+	public void frontInv(Cube cube){
+
+	}
+
+	public void right(Cube cube){
+
+	}
+
+	public void rightInv(Cube cube){
+
+	}
+
+	public void left(Cube cube){
+
+	}
+
+	public void leftInv(Cube cube){
+
+	}
+
+	public void up(Cube cube){
+
+	}
+
+	public void upInv(Cube cube){
+
+	}
+
+	public void down(Cube cube){
+
+	}
+
+	public void downInv(Cube cube){
+
+	}
+
+	private void rotate(int side, boolean clockwise){
+
+	}
+}
