dcb35ab2c3630beb1ff740f49619ef587f41cb5c
David Freeman <davidfreeman@kfc-48-105.dhcp.ksu.edu>
2016-02-19 08:49:43 -0600

    added InOut

---
+++ InOut.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class InOut{
+
+	public void println(String output){
+
+		System.out.println(output);
+	}
+
+	public void print(String output){
+
+		System.out.print(output);
+	}
+}
